f02329d68f271fa711059b8b59cc3bb49c3c5a63
LiecardoAntonio <liecardoantonio24@gmail.com>
2024-10-23 21:50:10 +0700

    progress1

---
+++ script.js
@@ -0,0 +1,12 @@
+//function to generate a range of numbers.
+const range = (start, end) => Array();
+
+window.onload = () => {
+  const container = document.getElementById('container');
+  const createLabel = (name) => {
+    const label = document.createElement('div');
+    label.className = 'label';
+    label.textContent = name;
+    container.appendChild(label);
+  };
+}
